e6c06b57b471fd3b7bd0cfe204238c91c7f5b58e
Chris Dower <chrisdower@chrisdower.xyz>
2018-06-26 09:24:20 -0700

    showdetail per tag

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ Vue.component('image-item', {
   template: '<tr> <td>{{ imagename }}</td> </tr>'
 })
 
+Vue.component('image-row', {
+  props: ['image'],
+  template: `
+    <tr> 
+      <td v-bind:title="image.clippy"> {{ item.name }} </td>
+      <td><button v-for="tag in image.tags">{{tag}}</button>>/td>
+      <div v-if="image.showdetail">Secret Sauce</div>
+    </tr>
+    `
+})
+
 var images = new Vue({ 
   el: '#images',
   data: {
@@ -19,15 +30,27 @@ var images = new Vue({
     var self = this;
     $.get( apiURL+'_catalog', function( data ) {
       for(var repo in data.repositories){
-        console.log(data.repositories[repo]);
+        //console.log(data.repositories[repo]);
         $.get( apiURL+data.repositories[repo]+'/tags/list', function( tags ) {
+          tags.id = repo;
           tags.clippy = "docker pull "+window.location.origin+'/'+tags.name+':'+tags.tags[0];
+          let tag_arr = []
+          for( let tag in tags.tags){
+	    let tag_obj = { 
+              name:       tags.tags[tag],
+              showdetail: false
+            }
+            tag_arr.push(tag_obj);
+          }
+          console.log(tag_arr);
+          tags.showdetail = false;
+          tags.tags = tag_arr;
           self.items.push(tags);
-          console.log(tags);
+          //console.log(tags);
         })
       }
       //self.items = data.repositories;
-      console.log(self.items);
+      //console.log(self.items);
     });
 
     }
